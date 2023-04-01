f9286e1bb8a932282d313358541c86f935072b2c
unknown <tasnia.turin534@gmail.com>
2023-04-01 23:13:39 +0600

    JS exam 1 added

---
+++ index.js
@@ -0,0 +1,47 @@
+let my_name = "Your Full Name";
+let name_length = my_name.length;
+console.log(name_length);
+
+let myArray = [3, 5, 9, 7, "ant", "Nure", [4, 5, 9]];
+let array_length = myArray.length;
+console.log(array_length);
+
+my_name = "Nure Siddiq";
+let e_index = my_name.indexOf('e');
+console.log(e_index);
+
+let my_number = "30";
+let number_sum = Number(my_number) + 40;
+console.log(number_sum);
+
+let my_identity = "nURe SiDDiQ";
+let upper_case_name = my_identity.toUpperCase();
+console.log(upper_case_name);
+
+let my_class = "nURe SiDDiQ";
+let lower_case_name = my_class.toLowerCase();
+console.log(lower_case_name);
+
+let myAge = "56";
+console.log(typeof myAge);
+
+let firstName = "Nure";
+let lastName = "Siddiq";
+console.log(firstName + " " + lastName);
+
+let currentDate = new Date();
+let month = currentDate.toLocaleString('default', { month: 'long' });
+let dayOfMonth = currentDate.getDate();
+let year = currentDate.getFullYear();
+
+console.log(month + " " + dayOfMonth + ", " + year);
+
+let yourAge = 25;
+console.log("I'm " + yourAge + " years old");
+
+
+
+
+
+
+
